fa301ac80abd2b6e83efcd272eaf0c2df4ef271e
Vitalii Mamontov <vintixfish@gmail.com>
2023-02-12 23:02:15 +0200

    Try to redirect to auth service

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ WORKDIR /app
 FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
 WORKDIR /src
 COPY ["GitHub.Statistics.API/GitHub.Statistics.API.csproj", "GitHub.Statistics.API/"]
-COPY ["GitHub.Statistics/GitHub.Statistics.csproj", "GitHub.Statistics/"]
+COPY ["GitHub.Statistics.API/GitHub.Statistics.API.csproj", "GitHub.Statistics.API/"]
 RUN dotnet restore "GitHub.Statistics.API/GitHub.Statistics.API.csproj"
 COPY . .
 WORKDIR "/src/GitHub.Statistics.API"
--- GitHub.Statistics.API/Controllers/TestController.cs
+++ GitHub.Statistics.API/Controllers/TestController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
 
 namespace GitHub.Statistics.API.Controllers
 {
@@ -13,9 +14,37 @@ namespace GitHub.Statistics.API.Controllers
             var httpclient = new HttpClient();
 
             var response = await httpclient.GetAsync("http://githubstatisticsauthentication-web-1:445/login");
-            var readResponse = await response.Content.ReadAsStringAsync();
+            var content = await response.Content.ReadAsStringAsync();
 
-            return Ok(readResponse);
+            var absoluteUri = response.RequestMessage.RequestUri.ToString().Split('?')[0];
+
+            Console.WriteLine("URL:" + absoluteUri);
+
+            var nameValues = HttpUtility.
+                ParseQueryString(response.RequestMessage.RequestUri.ToString().Split('?')[1]);
+            Console.WriteLine("Query: " + response.RequestMessage.RequestUri.Query);
+            // return_to parse to correct URI: /signin-github-token
+            foreach (var item in nameValues.AllKeys)
+            {
+                Console.WriteLine("Key: " + item.ToString());
+            }
+            //var key = nameValues.AllKeys[2];
+            //Console.WriteLine("Value:" + nameValues[key]);
+            //nameValues[key] = "http://localhost:2509/signin-github-token";
+            //Console.WriteLine("Value:" + nameValues[key]);
+
+            var url = absoluteUri + "?" + nameValues;
+
+            Console.WriteLine(url);
+            return Redirect(url);
+        }
+
+        [HttpGet]
+        [Route("setToken")]
+        public async Task<IActionResult> SetToken([FromQuery(Name = "token")] string token)
+        {
+            Console.WriteLine(token);
+            return Ok(token);
         }
     }
 }
